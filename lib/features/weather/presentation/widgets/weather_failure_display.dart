be8190c67a5acd1471cfed393f5877e7c44943e1
Bismus <alexeybismus@gmail.com>
2025-01-26 01:28:44 +0300

    add path to assets folders in pubspec

--- lib/features/weather/presentation/widgets/weather_failure_display.dart
+++ lib/features/weather/presentation/widgets/weather_failure_display.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class WeatherFailureDisplay extends StatelessWidget {
   const WeatherFailureDisplay({super.key, required this.failureMessage});
